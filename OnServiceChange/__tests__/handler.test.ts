675d8841e685a935072dea969db68cd96bbc23d5
LoFrance <lorenzo.franceschini@pagopa.it>
2022-09-21 20:41:58 +0200

    [IOPSC-57] Add createUpsertSql on handler OnServiceChange

--- OnServiceChange/__tests__/handler.test.ts
+++ OnServiceChange/__tests__/handler.test.ts
@@ -1,5 +1,8 @@
 import { Context } from "@azure/functions";
+import { MigrationRowDataTable } from "../../models/Domain";
+import { IDecodableConfigPostgreSQL } from "../../utils/config";
 import OnServiceChangeHandler from "../handler";
+import { createUpsertSql } from "../handler";
 
 describe("Handler", () => {
   it("should return a Function", async () => {
@@ -8,5 +11,29 @@ describe("Handler", () => {
       (void 0 as unknown) as any
     );
     expect(handler).toBeInstanceOf(Function);
+  });
+});
+
+describe("createUpsertSql", () => {
+  it("should compose correct upsert sql", async () => {
+    const config = {
+      DB_SCHEMA: "ServiceData",
+      DB_TABLE: "Export"
+    } as IDecodableConfigPostgreSQL;
+    const data = ({
+      subscriptionId: "subId1",
+      isVisible: true,
+      organizationFiscalCode: "12345678901",
+      sourceId: "00000000000000000000000000",
+      sourceName: "source name",
+      sourceSurname: "source surname",
+      sourceEmail: "source email",
+      serviceVersion: 0,
+      serviceName: "Service Test"
+    } as unknown) as MigrationRowDataTable;
+    const expected = `insert into "ServiceData"."Export" ("isVisible", "organizationFiscalCode", "serviceName", "serviceVersion", "sourceEmail", "sourceId", "sourceName", "sourceSurname", "subscriptionId") values (true, '12345678901', 'Service Test', 0, 'source email', '00000000000000000000000000', 'source name', 'source surname', 'subId1') on conflict ("subscriptionId") do update set "organizationFiscalCode" = excluded."organizationFiscalCode", "serviceVersion" = excluded."serviceVersion", "serviceName" = excluded."serviceName" where "Export"."serviceVersion" < excluded."serviceVersion"`;
+
+    const sql = createUpsertSql(config)(data);
+    expect(sql.trim()).toBe(expected.trim());
   });
 });
--- OnServiceChange/handler.ts
+++ OnServiceChange/handler.ts
@@ -1,8 +1,28 @@
 import { pipe } from "fp-ts/lib/function";
 import * as T from "fp-ts/lib/Task";
 import { Context } from "@azure/functions";
+import knex from "knex";
+import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import { MigrationRowDataTable } from "../models/Domain";
+import { IDecodableConfigPostgreSQL } from "../utils/config";
 
 type Handler = () => Promise<void>;
+
+export const createUpsertSql = (dbConfig: IDecodableConfigPostgreSQL) => (
+  data: MigrationRowDataTable
+): NonEmptyString =>
+  knex({
+    client: "pg"
+  })
+    .withSchema(dbConfig.DB_SCHEMA)
+    .table(dbConfig.DB_TABLE)
+    .insert(data)
+    .onConflict("subscriptionId")
+    .merge(["organizationFiscalCode", "serviceVersion", "serviceName"])
+    .whereRaw(
+      `"${dbConfig.DB_TABLE}"."serviceVersion" < excluded."serviceVersion"`
+    )
+    .toQuery() as NonEmptyString;
 
 // TO DO: This is the Handler and it's to be implemented!
 const handler = (): Handler =>
