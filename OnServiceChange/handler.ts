7eefb2276d55df1da9eeb84efd686ecd3614bb09
LoFrance <lorenzo.franceschini@pagopa.it>
2022-09-22 15:09:06 +0200

    [IOPSC-57] Add function to retrieve Owner from Subscription Id

--- OnServiceChange/handler.ts
+++ OnServiceChange/handler.ts
@@ -1,19 +1,26 @@
-import { pipe } from "fp-ts/lib/function";
+import { flow, pipe } from "fp-ts/lib/function";
 import * as E from "fp-ts/lib/Either";
 import * as O from "fp-ts/lib/Option";
 import * as T from "fp-ts/lib/Task";
+import * as TE from "fp-ts/lib/TaskEither";
 import * as RA from "fp-ts/lib/ReadonlyArray";
 import { Context } from "@azure/functions";
 import knex from "knex";
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { RetrievedService } from "@pagopa/io-functions-commons/dist/src/models/service";
+import { ErrorResponse as ApimErrorResponse } from "@azure/arm-apimanagement";
 import { MigrationRowDataTable } from "../models/Domain";
 import { IDecodableConfigPostgreSQL } from "../utils/config";
 import {
   ApimDelegateUserResponse,
-  ApimSubscriptionResponse
+  ApimSubscriptionResponse,
+  IApimConfig
 } from "../models/DomainApim";
-
+import {
+  IApimSubError,
+  toApimSubError,
+  toApimSubErrorMessage
+} from "../models/DomainErrors";
 type Handler = () => Promise<void>;
 
 /*
@@ -38,6 +45,44 @@ export const parseOwnerIdFullPath = (
         })
       )
     )
+  );
+
+export const getApimOwnerIdBySubscriptionId = (
+  apim: IApimConfig,
+  subscriptionId: NonEmptyString
+): TE.TaskEither<IApimSubError, ApimSubscriptionResponse> =>
+  pipe(
+    TE.tryCatch(
+      () =>
+        apim.client.subscription.get(
+          apim.config.APIM_RESOURCE_GROUP,
+          apim.config.APIM_SERVICE_NAME,
+          subscriptionId
+        ),
+      error =>
+        error as ApimErrorResponse & {
+          readonly statusCode?: number;
+        }
+    ),
+
+    TE.mapLeft(flow(toApimSubErrorMessage, toApimSubError)),
+
+    TE.chain(subscriptionGetResponse =>
+      flow(
+        NonEmptyString.decode,
+        E.mapLeft(_ => toApimSubError("Invalid Owner Id.")),
+        E.map(parseOwnerIdFullPath),
+        E.chainW(
+          E.fromOption(() => toApimSubError("Invalid Owner Id Full Path."))
+        ),
+        TE.fromEither
+      )(subscriptionGetResponse.ownerId)
+    ),
+
+    TE.map(ownerId => ({
+      ownerId,
+      subscriptionId
+    }))
   );
 
 export const mapDataToTableRow = (
