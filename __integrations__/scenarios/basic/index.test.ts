ce79b42f73e188e772dd1cf71abe17d67f2bac2c
Emanuele De Cupis <ema.decu@gmail.com>
2022-09-30 17:01:42 +0200

    add test to info endpoint

--- __integrations__/scenarios/basic/index.test.ts
+++ __integrations__/scenarios/basic/index.test.ts
@@ -1,3 +1,21 @@
+import fetch from "node-fetch";
+import { delay } from "../../utils/misc";
+
+jest.setTimeout(1e6);
+
+const myAppHost = `http://localhost:9090`;
+
+beforeAll(async () => {
+  await delay(20000);
+});
+
 describe("Basic", () => {
   it("should startup", () => expect(1).toBe(1));
+
+  it("should expose with application info ", async () => {
+    const response = await fetch(`${myAppHost}/api/v1/info`);
+    const info = await response.json();
+
+    expect(info).toEqual({});
+  });
 });
