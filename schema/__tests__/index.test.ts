a6728dd745d0d109d86e48ed25ed8b4a16cea7f6
LoFrance <lorenzo.franceschini@pagopa.it>
2022-11-15 18:00:51 +0100

    [#IOPSC-113] Add a simple test

---
+++ schema/__tests__/index.test.ts
@@ -0,0 +1,43 @@
+import { Client } from "pg";
+import * as env from "./env";
+
+const client = new Client({
+  host: env.DB_HOST,
+  user: env.DB_USER,
+  password: env.DB_PASSWORD,
+  database: env.DB_NAME
+});
+
+beforeAll(async () => {
+  await client.connect();
+});
+
+afterAll(async () => {
+  await client.end();
+});
+
+const aRandomSubscriptionId = () => `a-sub-id-${Date.now()}`;
+
+// Dummy insert for a given subscriptionId
+const aMigrationInsertSQL = (subscriptionId: string) => `
+    INSERT INTO "${env.DB_SCHEMA}"."${env.DB_NAME}"(
+        "id", "organizationFiscalCode", "version", "name", "isVisible", "requireSecureChannels", "subscriptionAccountId", "subscriptionAccountName", "subscriptionAccountSurname", "subscriptionAccountEmail")
+        VALUES ('${subscriptionId}', '0000000000', 1, 'any name', false, false, 1, 'aName', 'aSurname','an@email.com')
+`;
+
+// Select one record by subscriptionId
+const aMigrationSelectSQL = (subscriptionId: string) => `
+   SELECT * FROM "${env.DB_SCHEMA}"."${env.DB_NAME}" WHERE "subscriptionId"='${subscriptionId}'
+`;
+
+describe("it should select the first row", async () => {
+  const subscriptionId = aRandomSubscriptionId();
+
+  await client.query(aMigrationInsertSQL(subscriptionId));
+
+  const {
+    rows: [{ id: subscriptionRetrieved }]
+  } = await client.query(aMigrationSelectSQL(subscriptionId));
+
+  expect(subscriptionRetrieved).toBe(subscriptionId);
+});
