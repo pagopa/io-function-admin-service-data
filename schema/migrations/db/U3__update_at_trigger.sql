695c626558a24eb8b096d1fa840dcb82ec2f7d4a
LoFrance <lorenzo.franceschini@pagopa.it>
2022-11-22 08:39:13 +0100

    [#IOPSC-113] Remove Migration file

--- schema/migrations/db/U3__update_at_trigger.sql
+++
@@ -1,3 +0,0 @@
-DROP TRIGGER IF EXISTS update_servicedata_updateAt_with_current_timestamp ON "${schemaName}".services CASCADE;
-
-DROP FUNCTION IF EXISTS "${schemaName}".set_current_timestamp_on_updateAt();
