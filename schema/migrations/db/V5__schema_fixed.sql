7461e913a18736ee7f7afc0b707d0e65f1c32910
LoFrance <lorenzo.franceschini@pagopa.it>
2022-12-15 14:34:25 +0100

    [#IOPSC-120] Refactor columns name and index

--- schema/migrations/db/V5__schema_fixed.sql
+++ schema/migrations/db/V5__schema_fixed.sql
@@ -1,25 +1,26 @@
 ALTER TABLE "${schemaName}".services
   ADD COLUMN "organizationName" character varying,
   ADD COLUMN "serviceId" character varying,
-  ADD COLUMN "serviceMetadata_scope" character varying,
-  ADD COLUMN "serviceMetadata_description" character varying,
+  ADD COLUMN "serviceMetadataScope" character varying,
+  ADD COLUMN "serviceMetadataDescription" character varying,
   ADD COLUMN "quality" smallint,
   ADD COLUMN "departmentName" character varying,
   ADD COLUMN "maxAllowedPaymentAmount" integer,
-  ADD COLUMN "serviceMetadata_address" character varying,
-  ADD COLUMN "serviceMetadata_appAndroid" character varying,
-  ADD COLUMN "serviceMetadata_appIos" character varying,
-  ADD COLUMN "serviceMetadata_cta" character varying,
-  ADD COLUMN "serviceMetadata_email" character varying,
-  ADD COLUMN "serviceMetadata_phone" character varying,
-  ADD COLUMN "serviceMetadata_category" character varying,
-  ADD COLUMN "serviceMetadata_pec" character varying,
-  ADD COLUMN "serviceMetadata_privacyUrl" character varying,
-  ADD COLUMN "serviceMetadata_supportUrl" character varying,
-  ADD COLUMN "serviceMetadata_tokenName" character varying,
-  ADD COLUMN "serviceMetadata_tosUrl" character varying,
-  ADD COLUMN "serviceMetadata_webUrl" character varying,
-  ADD COLUMN "serviceMetadata_customSpecialFlow" character varying;
+  ADD COLUMN "serviceMetadataAddress" character varying,
+  ADD COLUMN "serviceMetadataAppAndroid" character varying,
+  ADD COLUMN "serviceMetadataAppIos" character varying,
+  ADD COLUMN "serviceMetadataCta" character varying,
+  ADD COLUMN "serviceMetadataEmail" character varying,
+  ADD COLUMN "serviceMetadataPhone" character varying,
+  ADD COLUMN "serviceMetadataCategory" character varying,
+  ADD COLUMN "serviceMetadataPec" character varying,
+  ADD COLUMN "serviceMetadataPrivacyUrl" character varying,
+  ADD COLUMN "serviceMetadataSupportUrl" character varying,
+  ADD COLUMN "serviceMetadataTokenName" character varying,
+  ADD COLUMN "serviceMetadataTosUrl" character varying,
+  ADD COLUMN "serviceMetadataWebUrl" character varying,
+  ADD COLUMN "serviceMetadataCustomSpecialFlow" character varying;
 
 -- to reach better performance for our query we decide to define an index on isVisible field to facilitate the retrieving only visible documents
-CREATE INDEX "isVisible_index" ON "${schemaName}".services ("isVisible");
+CREATE INDEX "isVisible_index" ON "${schemaName}".services USING hash ("isVisible")
+WHERE "isVisible" is true;
