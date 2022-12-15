c2d41518cc6957465f9419273732be71c68f5192
LoFrance <lorenzo.franceschini@pagopa.it>
2022-12-15 16:07:01 +0100

    [#IOPSC-120] Refactor with JSON type for optional service metadata

--- schema/migrations/db/V5__schema_fixed.sql
+++ schema/migrations/db/V5__schema_fixed.sql
@@ -1,25 +1,12 @@
 ALTER TABLE "${schemaName}".services
   ADD COLUMN "organizationName" character varying,
   ADD COLUMN "serviceId" character varying,
-  ADD COLUMN "serviceMetadataScope" character varying,
-  ADD COLUMN "serviceMetadataDescription" character varying,
+  ADD COLUMN "scope" character varying,
+  ADD COLUMN "description" character varying,
   ADD COLUMN "quality" smallint,
   ADD COLUMN "departmentName" character varying,
   ADD COLUMN "maxAllowedPaymentAmount" integer,
-  ADD COLUMN "serviceMetadataAddress" character varying,
-  ADD COLUMN "serviceMetadataAppAndroid" character varying,
-  ADD COLUMN "serviceMetadataAppIos" character varying,
-  ADD COLUMN "serviceMetadataCta" character varying,
-  ADD COLUMN "serviceMetadataEmail" character varying,
-  ADD COLUMN "serviceMetadataPhone" character varying,
-  ADD COLUMN "serviceMetadataCategory" character varying,
-  ADD COLUMN "serviceMetadataPec" character varying,
-  ADD COLUMN "serviceMetadataPrivacyUrl" character varying,
-  ADD COLUMN "serviceMetadataSupportUrl" character varying,
-  ADD COLUMN "serviceMetadataTokenName" character varying,
-  ADD COLUMN "serviceMetadataTosUrl" character varying,
-  ADD COLUMN "serviceMetadataWebUrl" character varying,
-  ADD COLUMN "serviceMetadataCustomSpecialFlow" character varying;
+  ADD COLUMN "serviceMetadata" json;
 
 -- to reach better performance for our query we decide to define an index on isVisible field to facilitate the retrieving only visible documents
 CREATE INDEX "isVisible_index" ON "${schemaName}".services USING hash ("isVisible")
