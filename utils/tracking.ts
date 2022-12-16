fc331d222a4481bfa59394b2f50d0c8b01ed6adb
Emanuele De Cupis <ema.decu@gmail.com>
2022-12-16 12:50:47 +0100

    [#IOPSC-127] add tracing utilities

--- utils/tracking.ts
+++ utils/tracking.ts
@@ -1,5 +1,20 @@
+import { RetrievedService } from "@pagopa/io-functions-commons/dist/src/models/service";
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import { pipe } from "fp-ts/lib/function";
 import { initTelemetryClient } from "./appinsight";
+
+type TelemetryClient = ReturnType<typeof initTelemetryClient>;
+
+/**
+ * Tracing utility that seamlessly return the subject of the tracing after a tracing function is applied
+ *
+ * @param tracer
+ * @returns
+ */
+export const trace = <T>(tracer: (subject: T) => void) => (subject: T): T => {
+  tracer(subject);
+  return subject;
+};
 
 /**
  * Track when a generic error occurred
@@ -7,9 +22,7 @@ import { initTelemetryClient } from "./appinsight";
  * @param telemetryClient
  * @returns
  */
-export const trackGenericError = (
-  telemetryClient: ReturnType<typeof initTelemetryClient>
-) => (
+export const trackGenericError = (telemetryClient: TelemetryClient) => (
   reason: string = "",
   obj: unknown = {} /** default empty object to prevent nullish values */
 ): void => {
@@ -30,7 +43,7 @@ export const trackGenericError = (
  * @returns
  */
 export const trackFailApimUserBySubscriptionResponse = (
-  telemetryClient: ReturnType<typeof initTelemetryClient>
+  telemetryClient: TelemetryClient
 ) => (
   reason: string = "",
   ownerId: NonEmptyString,
@@ -53,9 +66,9 @@ export const trackFailApimUserBySubscriptionResponse = (
  * @param telemetryClient
  * @returns
  */
-export const trackFailDecode = (
-  telemetryClient: ReturnType<typeof initTelemetryClient>
-) => (reason: string = ""): void => {
+export const trackFailDecode = (telemetryClient: TelemetryClient) => (
+  reason: string = ""
+): void => {
   telemetryClient.trackEvent({
     name: "developerportal.servicedata.fail-decode",
     properties: {
@@ -64,3 +77,62 @@ export const trackFailDecode = (
     tagOverrides: { samplingEnabled: "false" }
   });
 };
+
+const serviceEvent = (eventName: string) => (
+  service: RetrievedService
+): Parameters<TelemetryClient["trackEvent"]>[0] => ({
+  name: eventName,
+  properties: {
+    id: service.id,
+    serviceId: service.serviceId,
+    version: service.version
+  },
+  tagOverrides: { samplingEnabled: "false" }
+});
+
+/**
+ * Trace an incoming document to be processed
+ *
+ * @param telemetryClient
+ * @returns
+ */
+export const trackIncomingServiceDocument = (
+  telemetryClient: TelemetryClient
+) => (service: RetrievedService): void =>
+  pipe(
+    service,
+    serviceEvent("developerportal.servicedata.incoming-service-document"),
+    telemetryClient.trackEvent
+  );
+
+/**
+ * Trace when an incoming service document is processed and imported in database
+ *
+ * @param telemetryClient
+ * @returns
+ */
+export const trackProcessedServiceDocument = (
+  telemetryClient: TelemetryClient
+) => (service: RetrievedService): void =>
+  pipe(
+    service,
+    serviceEvent("developerportal.servicedata.processed-service-document"),
+    telemetryClient.trackEvent
+  );
+
+/**
+ * Trace failures on processing an incoming service document
+ *
+ * @param telemetryClient
+ * @returns
+ */
+export const trackFailedServiceDocumentProcessing = (
+  telemetryClient: TelemetryClient
+) => (service: RetrievedService): void =>
+  pipe(
+    service,
+    serviceEvent(
+      "developerportal.servicedata.failed-service-document-processing"
+    ),
+    telemetryClient.trackEvent
+  );
