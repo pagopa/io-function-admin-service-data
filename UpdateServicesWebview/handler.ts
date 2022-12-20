85aa6f0ad26983233dcdfae03cbb07dc555969e3
Rodolfo Viti <rodolfo.viti@pagopa.it>
2022-12-20 20:25:48 +0100

    [#IOPSC-123] defined service model structures

--- UpdateServicesWebview/handler.ts
+++ UpdateServicesWebview/handler.ts
@@ -1,19 +1,69 @@
 import { Context } from "@azure/functions";
+import { ServiceScopeEnum } from "@pagopa/io-functions-commons/dist/generated/definitions/ServiceScope";
+import { ServiceScope } from "@pagopa/io-functions-commons/dist/generated/definitions/ServiceScope";
 import { pipe } from "fp-ts/lib/function";
+import * as t from "io-ts";
 import * as TE from "fp-ts/TaskEither";
 import { Pool, PoolClient } from "pg";
 import * as Cursor from "pg-cursor";
+import { withDefault } from "@pagopa/ts-commons/lib/types";
+import {
+  NonEmptyString,
+  OrganizationFiscalCode
+} from "@pagopa/ts-commons/lib/strings";
 import { IConfig } from "../utils/config";
 import { initTelemetryClient } from "../utils/appinsight";
 
-interface ServiceRecord {
-  readonly id: string;
-}
-type Services = ReadonlyMap<ServiceRecord["id"], ServiceRecord>;
-interface CompactServices {}
-interface ExtendedServices {}
+export const ServiceRecord = t.intersection([
+  t.interface({
+    id: NonEmptyString,
+    name: NonEmptyString,
+    organizationFiscalCode: OrganizationFiscalCode,
+    organizationName: NonEmptyString,
+    quality: t.number,
+    scope: withDefault(ServiceScope, ServiceScopeEnum.NATIONAL)
+  }),
+  t.partial({
+    description: NonEmptyString
+  })
+]);
+export type ServiceRecord = t.TypeOf<typeof ServiceRecord>;
+type Services = ReadonlyMap<
+  ServiceRecord["organizationFiscalCode"],
+  ReadonlyArray<ServiceRecord>
+>;
 
-const createSQL = (): string => ``;
+interface ICompactService {
+  // Service Id
+  readonly i: string;
+  // Service name
+  readonly n: string;
+  // Quality flag, can be 1 or 0.
+  // 0. required quality level not reached
+  // 1. quality level reached
+  readonly q: number;
+}
+type CompactServices = ReadonlyArray<Organization<ICompactService>>;
+
+interface IExtendedService extends ICompactService {
+  // Service scope
+  readonly sc: ServiceScopeEnum;
+  // Service description
+  readonly d?: string;
+}
+type ExtendedServices = ReadonlyArray<IOrganization<IExtendedService>>;
+
+interface IOrganization<T extends ICompactService> {
+  // Organization Fiscal Code
+  readonly fc: string;
+  // Organization Name
+  readonly o: string;
+  // Organization services
+  readonly s: ReadonlyArray<T>;
+}
+
+const createSQL = (): string =>
+  `SELECT id, name, quality, scope, description, organizationFiscalCode, organizationName FROM <table> WHERE isVisible is true`;
 const createCursor = (pgClient: PoolClient) => (sql: string): Cursor =>
   pgClient.query(new Cursor(sql));
 
